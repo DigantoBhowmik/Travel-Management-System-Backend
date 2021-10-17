fcaf72d96ee4075ec9b0f39e7c4c5ea114876c71
Diganto Bhowmik <digantobhowmik@gmail.com>
2021-10-17 19:18:35 +0600

    Navbar Added

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="{{asset('asset/css/style.css')}}">
+<body>
+    <div >
+        @include('inc.navbar')
+
+        
+    </div>
+
+
+</body>
+</html>
